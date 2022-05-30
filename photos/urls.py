84359947de400b0f6d341730956d743a51d5439f
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-30 18:40:38 +0300

    add changes

--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpattern =[
+urlpatterns =[
     path('', views.gallery, name='gallery'),
     path('add/', views.addPhoto, name='add'),
     path('photo/<str:pk>/', views.viewPhoto, name='photo'),
