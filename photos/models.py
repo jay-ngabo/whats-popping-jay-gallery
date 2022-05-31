e39116aa288ce5b3b4531d5fafb82579092e362a
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-31 15:36:23 +0300

    add changes

--- photos/models.py
+++ photos/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-
 # Create your models here.
 
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     def __str__(self):
         return self.name
     
     
-class photo(models.Model):
-    category = models.ForeignKey(category, on_delete=models.SET_NULL, null=True)
+class Photo(models.Model):
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
-    description = models.TextField(max_length=500, null=False, blank=False)
+    description = models.TextField()
     def __str__(self):
         return self.description 
--- photoshare/settings.py
+++ photoshare/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'africa/nairobi'
 
 USE_I18N = True
 
@@ -119,7 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
+STATICFILE_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL_ROOT = BASE_DIR / 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
