d3a929e1da47baff0e56f717b9599b0d5b6674e6
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-31 11:52:02 +0300

    add changes

--- photos/urls.py
+++ photos/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns =[
     path('', views.gallery, name='gallery'),
+    path('photo/<str:pk>', views.viewPhoto, name='photo'),
     path('add/', views.addPhoto, name='add'),
-    path('photo/<str:pk>/', views.viewPhoto, name='photo'),
+    
 ]
--- photos/views.py
+++ photos/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 def gallery(request):
     return render(request, 'photos/gallery.html')
 
+def viewPhoto(request, pk):
+    return render(request, 'photos/photo.html')
+
+
 def addPhoto(request):
     return render(request, 'photos/add.html')
 
-def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')
