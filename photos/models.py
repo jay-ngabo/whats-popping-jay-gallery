e35571acaf02daa6e25631ce19462fb50a26d6a4
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-31 14:53:55 +0300

    add changes

--- photos/models.py
+++ photos/models.py
@@ -3,6 +3,14 @@ from django.db import models
 # Create your models here.
 
 class category(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)) 
+    name = models.CharField(max_length=50, null=False, blank=False)
     def __str__(self):
         return self.name
+    
+    
+class photo(models.Model):
+    category = models.ForeignKey(category, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField(max_length=500, null=False, blank=False)
+    def __str__(self):
+        return self.description 
